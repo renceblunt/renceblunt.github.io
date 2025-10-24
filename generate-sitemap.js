6a01818513091aa7fbd8c03adec42470e2c95602
volantpoetry <renceblunt@gmail.com>
2025-10-24 01:47:46 +0000

    50

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -11,7 +11,7 @@ const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
 // ---- CONFIG ----
-const domain = 'https://renceblunt.github.io';
+const domain = 'https://volantpoetry.github.io';
 const publicFolder = './';
 const firebaseKeyPath = './serviceAccountKey.json';
 
